9cfa57d2a13c21d05ce1d951fd13584ac473f0c1
vigo44 <vvigo@rambler.ru>
2022-10-16 23:30:21 +0400

    add: binary-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,41 +8,171 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor() {
+    this.three = null;
+  }
+  
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.three;
   }
-
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  add(data) {
+    this.three = addData(this.three, data);
+  
+    function addData(branch, value) {
+      if (!branch) {
+        branch = new Node (value);
+        return branch;
+      }
+      
+      
+      if (branch.data === value){
+        return branch;
+      }
+      
+      if (value < branch.data){
+        branch.left = addData (branch.left, value);
+        return branch;
+  
+      }
+  
+      if (value > branch.data){
+        branch.right = addData (branch.right, value);
+        return branch;
+      }
+    }
+  
   }
-
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  has(data) {
+    return hasData(this.three, data)
+  
+    function hasData(branch, value){
+      if (!branch) {
+        return false;
+      }
+      if (branch.data === value){
+        return true;
+      }
+      if (value < branch.data){
+        
+        return hasData(branch.left, value);  
+      }
+  
+      if (value > branch.data){
+        
+        return hasData(branch.right, value);  
+      }
+    }
+  
+  
   }
-
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  find(data) {
+    return findData (this.three, data);
+  
+    function findData (branch, value){
+      if (!branch) {
+        return null;
+      }
+      if (branch.data === value){
+        return branch;
+      }
+      if (value < branch.data){
+        
+        return findData(branch.left, value);  
+      }
+  
+      if (value > branch.data){
+        
+        return findData(branch.right, value);  
+      }
+    };
+    
   }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  remove(data) {
+    this.three = removeData(this.three, data)
+    
+    
+     
+  
+    function removeData(branch, value){
+      if (!branch) {
+        return null;
+      }
+      if (branch.data === value) {
+                
+  
+        if ((branch.left === null) && (branch.right === null) ){
+          return null;
+        }
+  
+        if ((branch.left === null)){
+          return branch.right;
+        }
+        if ((branch.right === null)){
+          return branch.left;
+      }
+    
+        let minRight = findMin(branch.right);
+        branch.data = minRight.data;
+        branch.right = removeData (branch.right, minRight.data)
+        return branch;
+     }  
+     if (value < branch.data){
+       branch.left = removeData(branch.left, value);  
+      return branch; 
+  
+    }
+  
+    if (value > branch.data){
+      branch.right = removeData(branch.right, value);  
+      return branch; 
+    }
+  
+    }
+  
+    function findMin(branch) {
+        if (branch.left === null) {
+          return branch
+        };
+        return findMin(branch.left);
+      
+    }
+  
+    
   }
-
+  
+  
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    function findMin(branch) {
+      if (!branch) {
+        return null;
+      }
+        if (branch.left === null) {
+          return branch
+        };
+        return findMin(branch.left);    
+   
   }
-
+   return findMin (this.three).data; 
+  }
+  
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    function findMax(branch) {
+      if (!branch) {
+        return null;
+      }
+        if (branch.right === null) {
+          return branch
+        };
+        return findMax(branch.right);    
+   
   }
-}
+   return findMax (this.three).data; 
+  }
+  }
 
 module.exports = {
   BinarySearchTree
